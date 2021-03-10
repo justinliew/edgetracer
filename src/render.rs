a779c9f3136e03f8db69c05da08d414a84d2ee2e
Justin Liew <jliew@fastly.com>
2021-03-09 22:29:44 -0800

    some local/edge iteration

--- src/render.rs
+++ src/render.rs
@@ -89,7 +89,7 @@ fn random_scene(seed: Option<u128>) -> HittableList {
 }
 
 #[derive(Clone,Copy)]
-struct ScreenPixel {
+pub struct ScreenPixel {
 	r: u8,
 	g: u8,
 	b: u8,
@@ -97,65 +97,85 @@ struct ScreenPixel {
 	y: usize,
 }
 
+const SAMPLES_PER_PIXEL : usize = 10;
+const MAX_DEPTH : usize = 10;
+
+const ASPECT_RATIO : f64 = 16.0/9.0;
+const WIDTH : usize = 400;
+const HEIGHT : usize = ((WIDTH as f64) / ASPECT_RATIO) as usize;
+
+const TILE_DIM : usize = 16;
+
+pub fn render_tile(thread_world: &HittableList, ti: usize, tj: usize) -> Vec<ScreenPixel> {
+	let mut ret : Vec<ScreenPixel> = Vec::new();
+	ret.reserve(TILE_DIM*TILE_DIM);
+
+	let camera = Camera::new(ASPECT_RATIO, 20.0, Point3::new(13.0,2.0,3.0), Point3::new(0.0,0.0,0.0), Point3::new(0.0,1.0,0.0));
+	let scale = 1.0 / (SAMPLES_PER_PIXEL as f64);
+
+	for j in 0..TILE_DIM {
+		for i in 0..TILE_DIM {
+			let mut pixel_colour = Colour::new(0.0,0.0,0.0);
+			for _ in 0..SAMPLES_PER_PIXEL {
+				let s = (((ti + i) as f64) + rand::random::<f64>()) / ((WIDTH-1) as f64);
+				let t = (((tj + j) as f64) + rand::random::<f64>()) / ((HEIGHT-1) as f64);
+				let r = &camera.get_ray(s,t);
+				let c = ray_colour(&r, thread_world, MAX_DEPTH);
+				pixel_colour = pixel_colour + c;
+			}
+			ret.push(ScreenPixel{r: (256.0 * clamp(f64::sqrt(pixel_colour.x * scale), 0.0, 0.999)) as u8,
+						g: (256.0 * clamp(f64::sqrt(pixel_colour.y * scale), 0.0, 0.999)) as u8,
+						b: (256.0 * clamp(f64::sqrt(pixel_colour.z * scale), 0.0, 0.999)) as u8,
+						x:ti+i,
+						y:tj+j});
+		}
+	}
+	ret
+}
+
 pub fn do_render() -> (u128, Vec<u8>) {
-	// TODO - add a timing param so it won't write colours, just calculate them
 
 	let start = Instant::now();
-
-	const SAMPLES_PER_PIXEL : usize = 10;
-	const MAX_DEPTH : usize = 10;
-
-	const ASPECT_RATIO : f64 = 16.0/9.0;
-	const WIDTH : usize = 400;
-	const HEIGHT : usize = ((WIDTH as f64) / ASPECT_RATIO) as usize;
-
 	let world = random_scene(None);
-
-	// Camera
 
 	// Render
 	let mut img: RgbImage = ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
-	let mut handles : Vec<thread::JoinHandle<Vec<ScreenPixel>>> = Vec::new();
-	handles.reserve((WIDTH*HEIGHT) / (16*16));
-	// we are nissing the top scanline
-	for tj in (0..HEIGHT-16).step_by(16).rev() {
-		for ti in (0..WIDTH).step_by(16) {
-				let thread_world = world.clone();
-				handles.push(thread::spawn(move || {
-					let mut ret : Vec<ScreenPixel> = Vec::new();
-					ret.reserve(16*16);
-					let camera = Camera::new(ASPECT_RATIO, 20.0, Point3::new(13.0,2.0,3.0), Point3::new(0.0,0.0,0.0), Point3::new(0.0,1.0,0.0));
-					let scale = 1.0 / (SAMPLES_PER_PIXEL as f64);
 
-					for j in 0..16 {
-						for i in 0..16 {
-							let mut pixel_colour = Colour::new(0.0,0.0,0.0);
-							for _ in 0..SAMPLES_PER_PIXEL {
-								let s = (((ti + i) as f64) + rand::random::<f64>()) / ((WIDTH-1) as f64);
-								let t = (((tj + j) as f64) + rand::random::<f64>()) / ((HEIGHT-1) as f64);
-								let r = &camera.get_ray(s,t);
-								let c = ray_colour(&r, &thread_world, MAX_DEPTH);
-								pixel_colour = pixel_colour + c;
-							}
-							ret.push(ScreenPixel{r: (256.0 * clamp(f64::sqrt(pixel_colour.x * scale), 0.0, 0.999)) as u8,
-										g: (256.0 * clamp(f64::sqrt(pixel_colour.y * scale), 0.0, 0.999)) as u8,
-										b: (256.0 * clamp(f64::sqrt(pixel_colour.z * scale), 0.0, 0.999)) as u8,
-										x:ti+i,
-										y:tj+j});
-						}
-					}
-					ret
-				}));
+	#[cfg(feature = "local")]
+	{
+		let mut handles : Vec<thread::JoinHandle<Vec<ScreenPixel>>> = Vec::new();
+		handles.reserve((WIDTH*HEIGHT) / (TILE_DIM*TILE_DIM));
+		// we are missing the top scanline
+		for tj in (0..HEIGHT-TILE_DIM).step_by(TILE_DIM).rev() {
+			for ti in (0..WIDTH).step_by(TILE_DIM) {
+					let thread_world = world.clone();
+					handles.push(thread::spawn(move || {
+						render_tile(&thread_world, ti,tj)
+					}));
+				}
+		}
+
+		for h in handles {
+			let p = h.join().unwrap();
+			for _ in 0..TILE_DIM*TILE_DIM {
+				for t in &p {
+					let pixel = img.get_pixel_mut(t.x as u32, (HEIGHT-t.y-1) as u32);
+					*pixel = image::Rgb([t.r, t.g, t.b]);
+				}
 			}
-//		println!("\r{} scanlines left", tj);
+		}
 	}
-
-	for h in handles {
-		let p = h.join().unwrap();
-		for _ in 0..16*16 {
-			for t in &p {
-				let pixel = img.get_pixel_mut(t.x as u32, (HEIGHT-t.y-1) as u32);
-				*pixel = image::Rgb([t.r, t.g, t.b]);
+	#[cfg(feature = "edge")]
+	{
+		for tj in (0..HEIGHT-TILE_DIM).step_by(TILE_DIM).rev() {
+			for ti in (0..WIDTH).step_by(TILE_DIM) {
+				let ret = render_tile(&world, ti, tj);
+				for _ in 0..TILE_DIM*TILE_DIM {
+					for t in &ret {
+						let pixel = img.get_pixel_mut(t.x as u32, (HEIGHT-t.y-1) as u32);
+						*pixel = image::Rgb([t.r, t.g, t.b]);
+					}
+				}
 			}
 		}
 	}
