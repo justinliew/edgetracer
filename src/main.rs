e152be784ca305b5b09c37e402d556e54dac5079
Justin Liew <jliew@fastly.com>
2021-03-13 17:27:28 -0800

    working version. Just need to fix it up after merge

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 mod vec3;
 mod ray;
 mod camera;
-mod dielectric;
 mod hittable;
-mod sphere;
+mod hitrecord;
 mod hittable_list;
 mod material;
-mod lambertian;
-mod metal;
 mod utils;
 mod render;
 
